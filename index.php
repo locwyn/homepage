a981753f49e0d809741bf6918e19512375be0553
locwyn <locwyn@protonmail.com>
2019-05-16 21:19:05 -0500

    testing php file reads

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?>
+    <?php echo '<h2>Seed Starter Temps</h2>'; ?>
+    <?php
+      $recordsArray = file('/home/gbk/data/weatherTracker/seedStarter/seedStarterTemps20170301.txt')
+      foreach($recordsArray as $line) {
+        echo $line;
+      }
   </body>
 </html>
